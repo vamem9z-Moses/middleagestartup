11743892b3cebc219cd90c8d7610d6cb3db2e1a0
Moses E Miles III <vamem9z@gmail.com>
2025-12-29 09:35:34 -0500

    Add posts scaffold

---
+++ db/migrate/20251229141726_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[8.1]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :body
+      t.boolean :published
+      t.datetime :publication_date
+      t.string :slug
+
+      t.timestamps
+    end
+  end
+end
