dfa73f24f1e32f5332c9208dac17282279f5520e
Moses E. Miles III <vamem9z@gmail.com>
2026-01-26 20:34:37 -0500

    Add Header

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,4 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@tailwindplus/elements", to: "@tailwindplus--elements.js" # @1.0.22
