1ba2eb7129eac30212a1344ff89360c5d8689bc5
Moses E. Miles III <vamem9z@gmail.com>
2026-01-31 12:20:10 -0500

    Fix tag colors on the index page

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,20 +1,16 @@
 module TagsHelper
   # https://tailwindcss.com/docs/detecting-classes-in-source-files#safelisting-classes
-  # Note: LEAVE THESE! They need to be scanned by tailwind for inclusion in the generated css.
-  # This is necessary for the colors to show since the colors are dynamically generated.
-  # text-pink bg-pink/8
-  # text-blue bg-blue/8
-  # text-cyan bg-cyan/8
-  # text-pink bg-pink/8
-  # text-green bg-green/8
-  # text-purple bg-purple/8
   def show_tags(tags)
     tags.map do |tag|
       num_posts = count_tag_usage(tag)
-      tag_colors = [ "blue", "green", "pink", "cyan", "purple" ]
-      tag_color = tag_colors.sample
+      dom_classes = [ "inline-flex text-red-800 bg-red-200/8 font-medium text-sm py-1 px-3 rounded-full",
+                    "inline-flex text-blue-800 bg-blue-200/8 font-medium text-sm py-1 px-3 rounded-full",
+                    "inline-flex text-purple-800 bg-purple-200/8 font-medium text-sm py-1 px-3 rounded-full",
+                    "inline-flex text-orange-800 bg-orange-200/8 font-medium text-sm py-1 px-3 rounded-full",
+                    "inline-flex text-green-800 bg-green-200/8 font-medium text-sm py-1 px-3 rounded-full"
+      ]
       tag_name = "#{tag.name} (#{num_posts})"
-      dom_class = "inline-flex text-green bg-#{tag_color}/8 font-medium text-sm py-1 px-3 rounded-full"
+      dom_class = dom_classes.sample
       link_to tag_name, tag,
         class: dom_class
     end.join.html_safe
